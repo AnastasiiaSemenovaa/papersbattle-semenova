1ec6b9c8dbc8d3c0d63f0da0b69391a289d26e23
Anastasiia Semenova <semenovaanastasiiaa@gmail.com>
2023-06-11 14:41:14 +0300

    Monica block

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+// import './libs./swiper-bundle.min.scss';
 import './style.scss';
+
 
 import lozad from 'lozad'
 const observer = lozad();
 observer.observe();
+
+var swiper = new Swiper(".swiper", {
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+    
+// document.addEventListener("DOMContentLoaded", function(event) {
+
+// }
+ 
