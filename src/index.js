6dfade60c0192a25d9bdd15c29ca4f74bf618699
Anastasiia Semenova <semenovaanastasiiaa@gmail.com>
2023-06-11 22:43:16 +0300

    worked again on dropdown with js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,61 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
 // import './libs./swiper-bundle.min.scss';
 import './style.scss';
+import Swiper, { Navigation, Pagination } from 'swiper';
+
 
 
 import lozad from 'lozad'
 const observer = lozad();
 observer.observe();
 
-var swiper = new Swiper(".swiper", {
-      spaceBetween: 30,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
-    
-// document.addEventListener("DOMContentLoaded", function(event) {
 
-// }
+    
+document.addEventListener("DOMContentLoaded", function(event) {
+  var swiper = new Swiper(".swiper", {
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+  let dropdown = document.querySelector('.dropdown-first');
+  let trigger = dropdown.querySelector('.header__link-trigger');
+  let header = document.querySelector('.header');
+
+  trigger.addEventListener('mouseenter', (event)=> {
+    // dropdown.classList.toggle('dropdown-first--active');
+    if (dropdown.classList.contains('dropdown-first--active')){
+      dropdown.classList.remove('dropdown-first--active');
+      
+    }
+    else {
+      dropdown.classList.add('dropdown-first--active');
+      header.style.backgroundColor="#fff";
+      headerColors();
+    }
+
+  });
+
+  // trigger.addEventListener('mouseleave', (event)=> {
+  //   header.style.backgroundColor="#9E77ED";
+  //   dropdown.classList.remove('dropdown-first--active');
+  // });
+
+
+
+  // function headerColors(){
+  //   let colorChange = document.querySelectorAll(".header__link-trigger");
+  //   colorChange.forEach(event => {
+  //     event.style.color="black";
+  //   })
+  // }
+
+
+
+
+
+
+});
  
